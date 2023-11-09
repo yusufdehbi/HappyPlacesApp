ef53722adaed124e269c1d2feef6c95ccaaf5d72
yusufdehbi <youssefdehbi59@gmail.com>
2023-11-09 15:44:23 +0100

    Adding the AddHappyPlaceActivity

--- app/src/main/java/com/dehbideveloper/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/dehbideveloper/happyplaces/AddHappyPlaceActivity.kt
@@ -2,10 +2,25 @@ package com.dehbideveloper.happyplaces
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dehbideveloper.happyplaces.databinding.ActivityAddHappyPlaceBinding
 
 class AddHappyPlaceActivity : AppCompatActivity() {
+
+    var binding: ActivityAddHappyPlaceBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_happy_place)
+        binding = ActivityAddHappyPlaceBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        setSupportActionBar(binding?.toolbarAddPlace)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding?.toolbarAddPlace?.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
--- app/src/main/java/com/dehbideveloper/happyplaces/MainActivity.kt
+++ app/src/main/java/com/dehbideveloper/happyplaces/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.dehbideveloper.happyplaces
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dehbideveloper.happyplaces.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-
+    var binding: ActivityMainBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.fabAddHappyPlace?.setOnClickListener{
+            val intent = Intent(this, AddHappyPlaceActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
