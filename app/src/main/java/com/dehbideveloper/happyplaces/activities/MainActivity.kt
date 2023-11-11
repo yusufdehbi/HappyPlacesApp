78dfe352388723a8e5c4c83dc60de8cf1738e8f1
yusufdehbi <youssefdehbi59@gmail.com>
2023-11-11 22:57:23 +0100

    Reading the data from local db and fix the database handler issue

--- app/src/main/java/com/dehbideveloper/happyplaces/activities/MainActivity.kt
+++ app/src/main/java/com/dehbideveloper/happyplaces/activities/MainActivity.kt
@@ -3,7 +3,10 @@ package com.dehbideveloper.happyplaces.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.dehbideveloper.happyplaces.database.DatabaseHandler
 import com.dehbideveloper.happyplaces.databinding.ActivityMainBinding
+import com.dehbideveloper.happyplaces.models.HappyPlaceModel
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +20,20 @@ class MainActivity : AppCompatActivity() {
         binding?.fabAddHappyPlace?.setOnClickListener{
             val intent = Intent(this, AddHappyPlaceActivity::class.java)
             startActivity(intent)
+        }
+
+        getHappyPlacesFromLocalDB()
+    }
+
+    private fun getHappyPlacesFromLocalDB() {
+        val dbHandler = DatabaseHandler(this)
+        val getHappyPlacesList: ArrayList<HappyPlaceModel> = dbHandler.getHappyPlacesList()
+
+        if (getHappyPlacesList.size > 0){
+            for (i in getHappyPlacesList){
+                Log.e("09876YY Title", i.title)
+                Log.e("09876YY Description", i.description)
+            }
         }
     }
 
