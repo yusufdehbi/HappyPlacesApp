a3db5691b3f1db669aca391f81128eecf7593ce7
yusufdehbi <youssefdehbi59@gmail.com>
2023-11-11 09:45:20 +0100

    handle storage and camera permission using dexter

--- app/src/main/java/com/dehbideveloper/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/dehbideveloper/happyplaces/AddHappyPlaceActivity.kt
@@ -1,14 +1,24 @@
 package com.dehbideveloper.happyplaces
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.DatePickerDialog
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.provider.CalendarContract.CalendarAlerts
+import android.provider.Settings
 import android.view.View
-import android.widget.DatePicker
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.dehbideveloper.happyplaces.databinding.ActivityAddHappyPlaceBinding
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import java.text.SimpleDateFormat
-import java.time.Year
 import java.util.Calendar
 import java.util.Locale
 
@@ -21,8 +31,10 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         binding = ActivityAddHappyPlaceBinding.inflate(layoutInflater)
         setContentView(binding?.root)
-        setSupportActionBar(binding?.toolbarAddPlace)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        setSupportActionBar(binding?.toolbarAddPlace) // Use the toolbar to set the action bar.
+        supportActionBar?.setDisplayHomeAsUpEnabled(true) // This is to use the home back button.
+        // Setting the click event to the back button
         binding?.toolbarAddPlace?.setNavigationOnClickListener {
             onBackPressed()
         }
@@ -33,12 +45,14 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
             cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
         }
         binding?.etDate?.setOnClickListener(this)
+        binding?.tvAddImage?.setOnClickListener(this)
     }
 
 
 
     override fun onClick(v: View?) {
         when(v!!.id){
+            // Handle click on the date EditText
             R.id.et_date -> {
                 DatePickerDialog(
                     this@AddHappyPlaceActivity,
@@ -47,9 +61,75 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
                     cal.get(Calendar.DAY_OF_MONTH)).show()
                 updateDateInView()
             }
+
+            // Handle click on the add image TextView
+            R.id.tv_add_image  -> {
+                val pictureDialog = AlertDialog.Builder(this)
+                pictureDialog.setTitle("Select Action")
+                val pictureDialogItems = arrayOf("Select photo from Gallery",
+                    "Capture photo from Camera")
+                pictureDialog.setItems(pictureDialogItems){
+                    _, which ->
+                    when(which){
+                        // Trigger the method to choose a photo from the Gallery
+                        0 -> choosePhotoFromGallery()
+                        // Display a message indicating that camera capture is not yet implemented
+                        1 -> Toast.makeText(this@AddHappyPlaceActivity, "Selecting photo from camera coming soon...", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                pictureDialog.show()
+            }
         }
     }
 
+
+    private fun choosePhotoFromGallery(){
+        // Use the Dexter library to request runtime permissions for reading and writing external storage and camera.
+        Dexter.withActivity(this).withPermissions(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        ).withListener(object: MultiplePermissionsListener {
+            override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                if (report!!.areAllPermissionsGranted()){
+                    // All required permissions are granted, show a toast message.
+                    Toast.makeText(this@AddHappyPlaceActivity,
+                        "Storage READ/WRITE are granted. Now you can select an image from Gallery", Toast.LENGTH_SHORT).show()
+                }
+            }
+            override fun onPermissionRationaleShouldBeShown(
+                permissions: MutableList<PermissionRequest>?,
+                permissionToken: PermissionToken?
+            ) {
+                // Display a rationale dialog to explain the need for permissions.
+                showRationalDialogForPermissions()
+            }
+        }).onSameThread().check()
+    }
+
+    //Redirect User to the Setting to grand the application access
+    private fun showRationalDialogForPermissions(){
+        AlertDialog.Builder(this)
+            .setMessage("It looks like you turned of permission required for this features it can" +
+                    " be enabled under the application settings")
+            .setPositiveButton("Go To Settings"){
+                _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException){
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel"){
+                dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
+    }
+
+    //put the date in the date EditText
     private fun updateDateInView(){
         val myFormat = "dd.MM.yyyy"
         val sdf = SimpleDateFormat(myFormat, Locale.getDefault())
