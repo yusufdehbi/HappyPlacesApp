f753356997844cba918000e573d809eb4cc7b0eb
yusufdehbi <youssefdehbi59@gmail.com>
2023-11-13 09:58:09 +0100

    Showing happy places as card elements in main screen with nice rounded image using third party library

--- app/src/main/java/com/dehbideveloper/happyplaces/activities/MainActivity.kt
+++ app/src/main/java/com/dehbideveloper/happyplaces/activities/MainActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.dehbideveloper.happyplaces.adapters.HappyPlacesAdapter
 import com.dehbideveloper.happyplaces.database.DatabaseHandler
 import com.dehbideveloper.happyplaces.databinding.ActivityMainBinding
 import com.dehbideveloper.happyplaces.models.HappyPlaceModel
@@ -25,15 +28,24 @@ class MainActivity : AppCompatActivity() {
         getHappyPlacesFromLocalDB()
     }
 
+    private fun setupHappyPlacesRecyclerView(happyPlaceList: ArrayList<HappyPlaceModel>){
+        binding?.rvHappyPlacesList?.layoutManager = LinearLayoutManager(this)
+        binding?.rvHappyPlacesList?.setHasFixedSize(true)
+        val placesAdapter = HappyPlacesAdapter(this, happyPlaceList)
+        binding?.rvHappyPlacesList?.adapter = placesAdapter
+    }
+
     private fun getHappyPlacesFromLocalDB() {
         val dbHandler = DatabaseHandler(this)
         val getHappyPlacesList: ArrayList<HappyPlaceModel> = dbHandler.getHappyPlacesList()
 
         if (getHappyPlacesList.size > 0){
-            for (i in getHappyPlacesList){
-                Log.e("09876YY Title", i.title)
-                Log.e("09876YY Description", i.description)
-            }
+            binding?.rvHappyPlacesList?.visibility = View.VISIBLE
+            binding?.tvNoRecordsAvailable?.visibility = View.GONE
+            setupHappyPlacesRecyclerView(getHappyPlacesList)
+        } else {
+            binding?.rvHappyPlacesList?.visibility = View.GONE
+            binding?.tvNoRecordsAvailable?.visibility = View.VISIBLE
         }
     }
 
---
+++ app/src/main/java/com/dehbideveloper/happyplaces/adapters/HappyPlacesAdapter.kt
@@ -0,0 +1,68 @@
+package com.dehbideveloper.happyplaces.adapters
+
+import android.content.Context
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.dehbideveloper.happyplaces.R
+import com.dehbideveloper.happyplaces.models.HappyPlaceModel
+import de.hdodenhof.circleimageview.CircleImageView
+
+open class HappyPlacesAdapter(
+    private val context: Context,
+    private var list: ArrayList<HappyPlaceModel>
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+
+    /**
+     * Inflates the item views which is designed in xml layout file
+     *
+     * create a new
+     * {@link ViewHolder} and initialzes some private fields to be used by RecyclerView
+     * */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return MyViewHolder(
+            LayoutInflater.from(context).inflate(
+                R.layout.item_happy_place,
+                parent,
+                false
+            )
+        )
+    }
+
+    /**
+     * Get the number of items in the list
+     * */
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    /**
+     * Binds each item in the ArrayList to a view
+     *
+     * Called when the recyclerview needs a new {@link ViewHolder} of the given to represent
+     * an item
+     *
+     * This new viewHolder should be contructed with a new View that can represent the items
+     * on the given type. You either create a new view manually or inflate it from an XML
+     * layout file
+     * */
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val model = list[position]
+
+        if (holder is MyViewHolder){
+            holder.itemView.findViewById<CircleImageView>(R.id.iv_place_image).setImageURI(Uri.parse(model.image))
+            holder.itemView.findViewById<TextView>(R.id.tvTitle).text = model.title
+            holder.itemView.findViewById<TextView>(R.id.tvDescription).text = model.description
+        }
+    }
+
+    /**
+     * A ViewHolder describes on item view and metadata about its place within ...
+     * */
+    private class MyViewHolder(view: View): RecyclerView.ViewHolder(view)
+
+}
