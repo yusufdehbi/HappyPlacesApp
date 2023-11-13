52baa6fbcf320f5cde8666927c74c6e8a18c86c4
yusufdehbi <youssefdehbi59@gmail.com>
2023-11-13 15:36:05 +0100

    Passing HappyPlaceModel Objects from MainActivity to HappyPlacesDetailActivity as serialazable

--- app/src/main/java/com/dehbideveloper/happyplaces/activities/HappyPlaceDetailActivity.kt
+++ app/src/main/java/com/dehbideveloper/happyplaces/activities/HappyPlaceDetailActivity.kt
@@ -1,10 +1,12 @@
 package com.dehbideveloper.happyplaces.activities
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dehbideveloper.happyplaces.R
 import com.dehbideveloper.happyplaces.databinding.ActivityHappyPlaceDetailBinding
 import com.dehbideveloper.happyplaces.databinding.ActivityMainBinding
+import com.dehbideveloper.happyplaces.models.HappyPlaceModel
 
 class HappyPlaceDetailActivity : AppCompatActivity() {
 
@@ -14,6 +16,26 @@ class HappyPlaceDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHappyPlaceDetailBinding.inflate(layoutInflater)
         setContentView(binding?.root)
+
+        var happyPlaceDetailModel: HappyPlaceModel? = null
+
+        if (intent.hasExtra(MainActivity.EXTRA_PLACE_DETAILS)){
+            happyPlaceDetailModel = intent.getSerializableExtra(MainActivity.EXTRA_PLACE_DETAILS) as HappyPlaceModel
+        }
+
+        if (happyPlaceDetailModel != null){
+            setSupportActionBar(binding?.toolbarHappyPlaceDetail)
+            supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+            supportActionBar!!.title = happyPlaceDetailModel.title
+
+            binding?.toolbarHappyPlaceDetail?.setNavigationOnClickListener {
+                onBackPressed()
+            }
+
+            binding?.ivPlaceImage?.setImageURI(Uri.parse(happyPlaceDetailModel.image))
+            binding?.tvDescription?.text = happyPlaceDetailModel.description
+            binding?.tvLocation?.text = happyPlaceDetailModel.location
+        }
 
     }
 
